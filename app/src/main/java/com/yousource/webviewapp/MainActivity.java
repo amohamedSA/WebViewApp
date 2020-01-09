3f06353f15c4f738f349945a6c12571f7535af2e
Aboebaker Mohamed <amohamed@you-source.co.za>
2020-01-09 13:47:36 +0200

    Updated Package

--- app/src/main/java/com/yousource/webviewapp/MainActivity.java
+++ app/src/main/java/com/yousource/webviewapp/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.webviewapp;
+package com.yousource.webviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -11,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://dev-cta-appservice-enlabeler-san.azurewebsites.net");
         WebSettings webSettings = webView.getSettings();
