d751ecb6d4022148d41dd34013a45476292c5460
Aboebaker Mohamed <amohamed@you-source.co.za>
2020-01-20 12:50:54 +0200

    Fixed popup issue and app logo

--- app/src/main/java/com/yousource/webviewapp/MainActivity.java
+++ app/src/main/java/com/yousource/webviewapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,13 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         webView = findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
+       /// webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://app.enlabeler.com/");
+        webView.setWebChromeClient(new WebChromeClient());
+
+
+
         WebSettings webSettings = webView.getSettings();
+
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setAppCacheEnabled(true);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
 
     }
 
